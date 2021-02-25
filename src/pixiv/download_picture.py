c893a126ec670954792ab09bc25c2da32ae109a2
wucy <741882354@qq.com>
2021-02-25 11:47:56 +0800

    封装下载器和多线程

--- src/pixiv/download_picture.py
+++ src/pixiv/download_picture.py
@@ -2,16 +2,22 @@ from image_data import ImageData
 import requests
 from bs4 import BeautifulSoup
 import json
-import _thread
+from src.utils.utils import *
+import threading
+
 
 class Pixiv():
 
-    def __init__(self, search, page, thread_number, star_number):
+    def __init__(self, search, page, star_number):
+        """
+
+        :param search: 搜索关键字
+        :param page: 搜索页码数
+        :param star_number: star数
+        """
         self.search = search
         self.page = page
-        #下载所使用的线程数
-        self.thread_number = thread_number
-        #设置抓取的图片须满足的点赞数量 
+        # 设置抓取的图片须满足的点赞数量
         self.star_number = star_number
         self.headers = {
             'X-Requested-With': 'XMLHttpRequest',
@@ -19,9 +25,9 @@ class Pixiv():
                           'Chrome/56.0.2924.87 Safari/537.36'}
         # 网页URL
         self.urls = []
-        # 图片信息
+        # 图片URL
         self.picture = []
-        # 筛选后的图片信息
+        # 图片信息
         self.result = []
 
     @property
@@ -42,7 +48,7 @@ class Pixiv():
               'type = all & lang = zh '
         urls = [fmt.format(self.search, self.search, p)
                 for p in range(1, self.page)]
-        if(self.page == 1):
+        if (self.page == 1):
             urls = [fmt.format(self.search, self.search, 1)]
         self.urls = urls
 
@@ -51,10 +57,10 @@ class Pixiv():
         获取全部图片URL
         :return:
         """
+        _count = 0
         while len(self.urls) > 0:
             url = self.urls.pop()
-            req = requests.get(url, headers=self.headers,
-                               cookies=self.cookies).text
+            req = requests.get(url, headers=self.headers,cookies=self.cookies).text
             new_data = json.loads(json.dumps(req))
             line = new_data
             # 处理json数据
@@ -69,15 +75,16 @@ class Pixiv():
             info = _dict['body']['illustManga']['data']
 
             for cnt in info:
-                # url = "https://www.pixiv.net/artworks/87860370"
                 self.picture.append(ImageData(cnt['id']))
-        print("共找到图片"+str(len(self.picture))+"张")
+                _count += 1
+        print(threading.current_thread().getName() + "共找到图片" + str(_count) + "张")
 
     def get_picture_info(self):
         """
         获取图片 收藏数 浏览量
         :return:
         """
+        _count = 0
         while len(self.picture) > 0:
             # 获取网页代码
             data = self.picture.pop()
@@ -94,55 +101,30 @@ class Pixiv():
                         meta['content'].replace("false", "'false'").replace("null", "'null'").replace("true",
                                                                                                       "'true'"))
                     if 'illust' in meta:
-                        #判断图片是否满足点赞数量的条件
-                        if(meta['illust'][pid]["likeCount"] >= self.star_number):
+                        # 判断图片是否满足点赞数量的条件
+                        if meta['illust'][pid]["likeCount"] >= self.star_number:
                             data.set_info(meta['illust'][pid]['urls']['original'], meta['illust'][pid]["title"],
-                            meta['illust'][pid]["userName"], meta['illust'][pid]["likeCount"],)
+                                          meta['illust'][pid]["userName"], meta['illust'][pid]["likeCount"], )
+                            download_picture(data['url'][0], data['pid'])
+                            # 保存图片信息
                             self.result.append(data)
-        print("其中符合条件的图片"+str(len(self.result))+"张")
-
-    def download(self):
-        """
-        多线程下载图片
-        :return:
-        """
-        print("开始下载")
-        for line_number in range(self.thread_number):
-            _thread.start_new_thread(self.download_line,(line_number,))
-
-    def download_line(self,line_number):
-        """
-        多线程下载图片
-        :param line_number: 线程名
-        :return:
-        """
-        print("下载线程"+str(line_number)+"启动")
-        while len(self.result) > 0 :
-            image_data = self.result.pop()
-            image = image_data.get_info()
-            self.download_picture(image['url'][0], image['pid'])
-        print("线程"+str(line_number)+"下载完毕")
-
-    def download_picture(self, url, pid):
-        """
-        图片下载器
-        :param url: 图片地址
-        :param id: 图片ID
-        :return:
-        """
-        name=str(pid) + ".jpg"
-        header={'Referer': 'https://www.pixiv.net/'}
-        req=requests.get(url, headers=header, stream=True)
-        if req.status_code == 200:
-            open('picture\\' + name, 'wb').write(req.content)  # 将内容写入图片
-        else:
-            print(name + "下载失败")
-        del req
+                            _count += 1
+        print(threading.current_thread().getName() + "共下载图片" + str(_count) + "张")
 
 
 if __name__ == "__main__":
-    spider=Pixiv("冬", 5, 3, 100)
+    thread_number = 2
+    spider = Pixiv(search="冬", page=3, star_number=100)
     spider.get_urls()
-    spider.run_get_picture_url()
-    spider.get_picture_info()
-    spider.download()
+    # 获取线程
+    thread_lst = []
+    # 启动多个线程 获取图片ID
+    for _ in range(thread_number):
+        t = create_thread(spider.run_get_picture_url)
+        thread_lst.append(t)
+    # 阻塞线程 等执行完后再去下载图片
+    for thread_ in thread_lst:
+        thread_.join()
+    # 获取合适图片 并下载
+    for _ in range(thread_number):
+        create_thread(spider.get_picture_info)

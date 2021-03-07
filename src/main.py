8497a4639b4873fdcbd720d34f7affd3f9bc8f87
wucy <741882354@qq.com>
2021-03-07 22:16:41 +0800

    定时任务完成

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from pixiv.search import PixivSearch
 from pixiv.recommand import pixiv_recommand
 from pixiv.daily import pixiv_daily
 from utils.logger import Logger
+from utils.util import create_thread
+import schedule
+import time
+import datetime
 
 logger = Logger("hmk").get_log()
 
@@ -34,9 +38,33 @@ def case3(cookie):
     spider.run()
 
 
+def job4():
+    print('Job4:每天下午17:49执行一次，每次执行20秒')
+    print('Job4-startTime:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    time.sleep(20)
+    print('Job4-endTime:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    print('------------------------------------------------------------------------')
+
+
+def job1():
+    print('Job1:每隔10秒执行一次的任务，每次执行2秒')
+    print('Job1-startTime:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    time.sleep(2)
+    print('Job1-endTime:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    print('------------------------------------------------------------------------')
+
+
+def dingshi():
+    schedule.every().day.at('12:00').do(job4)
+    # schedule.every(10).seconds.do(job1)
+    while True:
+        schedule.run_pending()
+
 if __name__ == "__main__":
     cookie = cookies()
+    create_thread(dingshi)
     while True:
+
         print('选择功能')
         print('1.搜索图片')
         print('2.获取推荐图片')
@@ -56,7 +84,7 @@ if __name__ == "__main__":
         }
 
         try:
-            logger.info("User is using function %s"%case)
+            logger.info("User is using function %s" % case)
             switch[case](cookie)
             logger.info("function %s end" % case)
         except KeyError as e:
